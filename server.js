1aa4a20855bdfa3c73b1641c18a0657971e257e6
Melissa Hale <melissakayh@gmail.com>
2023-07-02 17:57:50 -0500

    log things

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const path = require('path');
 const gateway = require('express-gateway');
+const morgan = require('morgan');
+
+const logger = morgan('combined');
 
 gateway()
   .load(path.join(__dirname, 'config'))
   .run({
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    pre: (app) => {
+      app.use(logger);
+    }
   });
