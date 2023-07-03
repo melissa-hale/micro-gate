f7ba0ad89f9b54cfe132fed7d3df3ce5faa05d17
Melissa Hale <melissakayh@gmail.com>
2023-07-02 19:47:22 -0500

    try

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 const path = require('path');
 const gateway = require('express-gateway');
-const morgan = require('morgan');
-
-const logger = morgan('combined', { stream: process.stdout });
 
 gateway()
   .load(path.join(__dirname, 'config'))
   .run({
     host: '::',
-    port: process.env.PORT,
-    pre: (app) => {
-      app.use(logger);
-    }
+    port: process.env.PORT
   });
